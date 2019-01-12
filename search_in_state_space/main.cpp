0bae994f36a41b360df0ba00ab3601beb9058d1d
Alan <kumaritof@ya.ru>
2019-01-13 00:46:34 +0300

    solve 4 task

--- search_in_state_space/main.cpp
+++ search_in_state_space/main.cpp
@@ -54,7 +54,7 @@ public:
 };
 
 
-Position* task12(int start, int end) {
+Position *task12(int start, int end) {
     set<Position *, Position::comparator> positions;
     queue<Position *> q;
     auto first = new Position(start, 0, nullptr);
@@ -77,7 +77,7 @@ Position* task12(int start, int end) {
     return nullptr;
 }
 
-Position* task3(int start, int end) {
+Position *task3(int start, int end) {
     set<Position *, Position::comparator> positions;
     queue<Position *> q;
     auto first = new Position(end, 0, nullptr);
@@ -103,13 +103,57 @@ Position* task3(int start, int end) {
     return nullptr;
 }
 
+pair<Position*, Position*> task4(int start, int end) {
+    set<Position *, Position::comparator> straight_positions;
+    set<Position *, Position::comparator> reverse_positions;
+    queue<pair<Position *, bool>> q;
+    auto head = new Position(start, 0, nullptr);
+    straight_positions.insert(head);
+    q.emplace(head, true);
+    auto tail = new Position(end, 0, nullptr);
+    straight_positions.insert(tail);
+    q.emplace(tail, false);
+    while (!q.empty()) {
+        bool is_straight = q.front().second;
+        auto current = q.front().first;
+        q.pop();
+        if (is_straight) {
+            for (const auto &f: funs) {
+                auto next = new Position(f(current->getNum()), current->getStep() + 1, current);
+                if (straight_positions.find(next) == straight_positions.end()) {
+                    if (reverse_positions.find(next) != reverse_positions.end()) {
+                        return {next, *reverse_positions.find(next)};
+                    }
+                    q.emplace(next, true);
+                    straight_positions.insert(next);
+                }
+            }
+        } else {
+            for (const auto &f: refuns) {
+                int num = current->getNum();
+                if (f(num)) {
+                    auto next = new Position(num, current->getStep() + 1, current);
+                    if (reverse_positions.find(next) == reverse_positions.end()) {
+                        if (straight_positions.find(next) != straight_positions.end()) {
+                            return {*straight_positions.find(next), next};
+                        }
+                        q.emplace(next, false);
+                        reverse_positions.insert(next);
+                    }
+                }
+            }
+        }
+    }
+    return {nullptr, nullptr};
+}
+
 int main() {
     int a, b;
     cout << "Enter start value: ";
     cin >> a;
     cout << "Enter finish value: ";
     cin >> b;
-    Position* answer = task3(a, b);
-    cout << answer->getStep();
+    auto answer = task4(a, b);
+    cout << answer.first->getStep() + answer.second->getStep();
     return 0;
 }
