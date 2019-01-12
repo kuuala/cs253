9b9c002da5c888f0034e0c88e9f8c7cb7f99d096
Alan <kumaritof@ya.ru>
2019-01-12 22:59:03 +0300

    solve 1&2 task

--- search_in_state_space/main.cpp
+++ search_in_state_space/main.cpp
@@ -1,9 +1,58 @@
 #include <iostream>
-#include <deque>
+#include <queue>
+#include <functional>
+#include <vector>
+#include <set>
 
-using std::cin;
-using std::cout;
-using std::endl;
+using namespace std;
+
+const vector<function<int(int)>> g_funs {
+    [] (int num) { return num * 2; },
+    [] (int num) { return num + 3; },
+    [] (int num) { return num - 2; }
+};
+
+class Position {
+    int num;
+    int step;
+    Position *parent;
+public:
+    Position(): num(0), step(0), parent(nullptr) {}
+    Position(int num, int step, Position *parent): num(num), step(step), parent(parent) {}
+    ~Position() { delete parent; }
+    int getNum() const { return num; }
+    int getStep() const { return step; }
+    Position *getPosition() const { return parent; }
+};
+
+struct comparator {
+    bool operator()(const Position *pos1, const Position *pos2) const {
+        return pos1->getNum() < pos2->getNum();
+    }
+};
+
+Position* task1(int start, int end) {
+    set<Position *, comparator> positions;
+    queue<Position *> q;
+    auto first = new Position(start, 0, nullptr);
+    positions.insert(first);
+    q.push(first);
+    while(!q.empty()) {
+        auto current = q.front();
+        q.pop();
+        if (current->getNum() == end) {
+            return current;
+        }
+        for (const auto& fun: g_funs) {
+            auto to = new Position(fun(current->getNum()), current->getStep() + 1, current);
+            if (positions.find(to) == positions.end()) {
+                q.push(to);
+                positions.insert(to);
+            }
+        }
+    }
+    return nullptr;
+}
 
 int main() {
     int a, b;
@@ -11,5 +60,7 @@ int main() {
     cin >> a;
     cout << "Enter finish value: ";
     cin >> b;
+    Position* answer = task1(a, b);
+    cout << answer->getStep();
     return 0;
 }
